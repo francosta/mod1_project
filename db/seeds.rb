43687e8d090aa2ca62b19a9f4979240a03aa4fd9
francosta <francisco@fcosta.pt>
2019-04-16 10:21:29 +0100

    added b on my branch

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@ require 'pry'
 # u1 = User.create(name: "Nahit", email: "nahit@gmail.com")
 # u2 = User.create(name: "Fran", email: "fran@gmail.com")
 #
-# q1 = Question.create(country: c1, category: cat1)
+# q1 = Question.create(country: c1, category: cat1)b
 # q2 = Question.create(country:c1, category:cat2)
 # q3 = Question.create(country:c2, category:cat1)
 # q4 = Question.create(country:c2, category:cat2)
