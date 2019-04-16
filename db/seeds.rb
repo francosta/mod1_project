3b8abe13d66121b4917f64999e789ea79e99d460
francosta <francisco@fcosta.pt>
2019-04-16 10:15:56 +0100

    deleted 'b' on seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,6 @@ require 'pry'
 #
 # q1 = Question.create(country: c1, category: cat1)
 # q2 = Question.create(country:c1, category:cat2)
-# q3 = Question.create(country:c2, category:cat1)b
+# q3 = Question.create(country:c2, category:cat1)
 # q4 = Question.create(country:c2, category:cat2)
 COUNTRIES_ARRAY.each {|country| Country.create(name: country[:name], code: country[:code])}
