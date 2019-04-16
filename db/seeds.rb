80d9553bf7c65835388e4a5a42dad7d2efdb1f10
francosta <francisco@fcosta.pt>
2019-04-16 09:28:15 +0100

    No changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,11 @@
 require 'pry'
+
 #
 # u1 = User.create(name: "Nahit", email: "nahit@gmail.com")
 # u2 = User.create(name: "Fran", email: "fran@gmail.com")
 #
 # q1 = Question.create(country: c1, category: cat1)
 # q2 = Question.create(country:c1, category:cat2)
-# q3 = Question.create(country:c2, category:cat1)
+# q3 = Question.create(country:c2, category:cat1)b
 # q4 = Question.create(country:c2, category:cat2)
-COUNTRIES_ARRAY.each {|country| Country.create(name: country.name, code: country.code)}
+COUNTRIES_ARRAY.each {|country| Country.create(name: country[:name], code: country[:code])}
