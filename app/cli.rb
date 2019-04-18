8d2b5fd2085d28bbd7b38171bd2083b4e4ea4acc
francosta <francisco@fcosta.pt>
2019-04-18 09:50:26 +0100

    More cleanup

--- app/cli.rb
+++ app/cli.rb
@@ -2,10 +2,12 @@ class CLI
 
   def initialize
     @prompt = TTY::Prompt.new
+    display_splash_text
   end
 
 # Displays the start menu
   def start_menu
+    puts ""
     choice = @prompt.select("Welcome! Please choose from the following options:") do |menu|
       menu.choice 'Play'
       menu.choice 'Account Management'
@@ -13,43 +15,62 @@ class CLI
       menu.choice "Exit"
     end
 
-  if choice == "Exit"
-    goodbye
-  elsif choice == "Play"
-    say_greeting
-    find_or_create_user
-    welcome
-    formulate_question
-  elsif choice == "Check Scoreboard"
-
-    existing_users_ids = User.all.map {|user| if user.email != "!!!" then user.id end}.compact
-
-    existing_users_names = User.all.map {|user| if user.email != "!!!" then user.name end}.compact
-
-    existing_users_questions =
-    existing_users_ids.map do |id|
-      Question.all.select {|question| question.user_id == id}
+  if choice == "Play"
+    if @user
+      play
+    else
+      login
+      play
     end
-
-    users_points = existing_users_questions.map {|user_questions| user_questions.length}
-    scoreboard = []
-    existing_users_names.each do |name|
-      array = []
-      array << name
-      array << users_points[existing_users_names.index(name)]
-      scoreboard << array
-    end
-
-    scoreboard = scoreboard.sort_by {|element| element[1]}.reverse
-
-    table = TTY::Table.new ['Player','Points'], scoreboard
-
-    puts table.render(:ascii)
-    sleep(2)
-    start_menu
-
   elsif choice == "Account Management"
     manage_account
+  elsif choice == "Check Scoreboard"
+    scoreboard
+  elsif choice == "Exit"
+    goodbye
+  end
+end
+
+def login
+  puts ""
+  puts "Please login to your account."
+  email = @prompt.ask("What's your email?")
+  if @user = User.find_by(email: email)
+    @user = User.find_by(email: email)
+    puts ""
+    password = @prompt.mask("Please insert your password:".green)
+    if @user.authenticate(password)
+      puts ""
+      puts "Welcome back #{@user.name}.".green
+      puts ""
+    else
+      puts ""
+      puts "This password is incorrect.".red
+      puts ""
+      password = @prompt.mask("Please reinsert your password:".red)
+      if @user.authenticate(password)
+        puts ""
+        puts "Welcome back #{@user.name}.".green
+        puts ""
+      else
+        puts ""
+        puts "Sorry, this password is still incorrect.".red
+        puts "Please go away and check your password!"
+        puts ""
+        system exit
+      end
+    end
+  else
+    puts "**************"
+    puts "We could not retrieve your account.".yellow
+    puts "It seems like you have never played!".yellow
+    puts "**************"
+    if @prompt.yes?("Would you like to create an account?")
+      puts ""
+      create_account
+    else
+      start_menu
+    end
   end
 end
 
@@ -58,88 +79,38 @@ end
     system 'say "Good Luck"'
   end
 
-# Retrieves user account through login or creates a new one
-  def find_or_create_user
-    puts ""
-    puts "Please login to your account."
-    email = @prompt.ask("What's your email?")
-    if @user = User.find_by(email: email)
-      @user = User.find_by(email: email)
-      puts ""
-      password = @prompt.mask("Please insert your password:".green)
-      if @user.authenticate(password)
-        puts "Welcome back #{@user.name}.".green
-        sleep (2)
-        puts ""
-        puts "Let's play!"
-        sleep(2)
-      else
-        puts ""
-        puts "This password is incorrect.".red
-        puts ""
-        password = @prompt.mask("Please reinsert your password:".red)
-        if @user.authenticate(password)
-          puts "Welcome back #{@user.name}.".green
-          sleep (1)
-          puts ""
-          puts "Let's play!".yellow
-          sleep(1)
-        else
-          puts ""
-          puts "Sorry, this password is still incorrect.".red
-          puts "Please go away and check your password!"
-          puts ""
-          system exit
-        end
-      end
-    else
-      puts "It seems like you have never played!
-      ".yellow
-      if @prompt.yes?("Would you like to create an account?")
-        "
-        Let's create your account!
-        ".green
-        name = @prompt.ask("What's your name?")
-        email = @prompt.ask("What's your email?")
-        password = @prompt.mask("Please create a password:")
-        password_reenter = @prompt.mask("Please confirm your password:")
-        if password == password_reenter
-          @user = User.new(name: name.titleize.strip, email: email, password: password)
-          @user.save
-          sleep(2)
-          puts ""
-          puts "Thanks for creating your account.".green
-          puts "Let's play!".yellow
-          puts ""
-        else
-          puts "The passwords do not match.".red
-          password = @prompt.mask("Please reenter your password:".red)
-          password_reenter = @prompt.mask("Please confirm your password:".red)
-          if password == password_reenter
-            @user = User.new(name: name.titleize.strip, email: email, password: password)
-            @user.save
-            sleep(2)
-            puts "
-            Thanks for creating an account.".green
-          else
-            "
-            The passwords do not match.
-            Please try again later.
-            ".red
-            system exit
-          end
-        end
-      else
-        puts ""
-        puts "Goodbye, then.".blue
-        puts ""
-        system exit
-      end
+# Produces and shows a scoreboard with all users sorted by number of points
+  def scoreboard
+    existing_users_ids = User.all.map {|user| if user.email != "!!!" then user.id end}.compact
+    existing_users_names = User.all.map {|user| if user.email != "!!!" then user.name end}.compact
+    existing_users_questions =
+    existing_users_ids.map do |id|
+      Question.all.select {|question| question.user_id == id}
     end
+    users_points = existing_users_questions.map {|user_questions| user_questions.length}
+    scoreboard = []
+    existing_users_names.each do |name|
+      array = []
+      array << name
+      array << users_points[existing_users_names.index(name)]
+      scoreboard << array
+    end
+    scoreboard = scoreboard.sort_by {|element| element[1]}.reverse
+    table = TTY::Table.new ['Player','Points'], scoreboard
+    puts table.render(:ascii)
+    sleep(2)
+    start_menu
+  end
+
+  def play
+    say_greeting
+    welcome
+    formulate_question
   end
 
 # Welcomes a user before starting a game session.
   def welcome
+    puts ""
     puts "Welcome, your score will be saved to #{@user.email}.".yellow
     puts ""
     puts "You have #{@user.questions.length} points."
@@ -148,146 +119,155 @@ end
 
 # Creates a question from the database and initiates a game round
   def formulate_question
+    @category = @user.available_questions.sample.keys
+    @category_instance = Category.all.select {|cat| cat.name == @category[0]}
+    category_hash = @user.available_questions.select {|h| h.keys == @category}
+    @country = category_hash[0].values[0].sample
+    @country_instance = Country.all.select {|cou| cou.name == @country}
+    pose_question
+  end
 
-  # formulate question
-    category = @user.available_questions.sample.keys
-    category_instance = Category.all.select {|cat| cat.name == category[0]}
-    category_hash = @user.available_questions.select {|h| h.keys == category}
+  def pose_question
+    @question = "#{@category_instance[0].text} #{@country}?"
+    puts "Category: ".blue + "#{@category[0].capitalize}"
+    puts "Question: ".blue + "#{@question}"
+    puts ""
+    update_user_questions
+  end
 
-    country = category_hash[0].values[0].sample
-    country_instance = Country.all.select {|cou| cou.name == country}
+  def update_user_questions
+    @user.update_questions(@category[0], @country)
+    get_correct_answer
+  end
 
-  # pose question
-    question = "#{category_instance[0].text} #{country}?"
-    puts "Category: ".blue + "#{category[0].capitalize}"
-    puts "Question: ".blue + "#{question}"
-
-  # update user questions
-    @user.update_questions(category[0], country)
-
-  # get correct answer
-    response_string = RestClient.get("https://restcountries.eu/rest/v2/alpha/#{country_instance[0].code}")
+  def get_correct_answer
+    response_string = RestClient.get("https://restcountries.eu/rest/v2/alpha/#{@country_instance[0].code}")
     country_info = JSON.parse(response_string)
 
-    if category_instance[0].name == "capital"
+    if @category_instance[0].name == "capital"
       answer = country_info["capital"].downcase
-    elsif category_instance[0].name == "currency"
-      answer = country_info["currencies"][0]["name"].downcase
+    elsif @category_instance[0].name == "currency"
+      @answer = country_info["currencies"][0]["name"].downcase
     else
-      answer = country_info["languages"][0]["name"].downcase
+      @answer = country_info["languages"][0]["name"].downcase
     end
+    get_user_answer
+  end
 
-  # get answer from user
+  def get_user_answer
     guess = gets.chomp.downcase.strip
-    if guess == answer
+    if guess == @answer
       puts ""
       puts "Well done, your score has increased by 1 point.".blue
-      Question.create(user_id: @user.id, category_id: category_instance[0].id, country_id: country_instance[0].id)
+      Question.create(user_id: @user.id, category_id: @category_instance[0].id, country_id: @country_instance[0].id)
       puts ""
       puts "You now have #{@user.questions.reload.length} points."
       puts ""
-      if @prompt.yes?("Would you like to continue playing?".blue)
-        formulate_question
-      else
-        puts ""
-        puts "Thanks for playing!".yellow
-        start_menu
-      end
+      continue_play?
     else
-      puts "
-      Unfortunately your answer was incorrect. The correct answer was #{answer}.".red
-      puts "
-      You have #{@user.questions.length} points.
-      "
-      if @prompt.yes?("Would you like to continue playing?".blue)
-        formulate_question
-      else
-        puts ""
-        puts "Thanks for playing!".yellow
-        start_menu
-      end
+      puts ""
+      puts "Unfortunately your answer was incorrect. The correct answer was #{@answer.titleize}.".red
+      puts ""
+      puts "You have #{@user.questions.length} points."
+      puts ""
+      continue_play?
     end
   end
 
-# Logs user in and shows Account Management menu.
-  def manage_account
-    sleep(1)
-    puts "Please login to your account".green
-    puts ""
-    email = @prompt.ask("What's your email?")
-    if @user = User.find_by(email: email)
-      @user = User.find_by(email: email)
-      password = @prompt.mask("Please insert your password:")
-      if @user.authenticate(password)
-        puts ""
-        puts "You've successfully logged in to your account.".green
-        puts ""
-        sleep(1)
-      else
-        puts "The password you entered is incorrect.".red
-        puts ""
-        sleep(1)
-        password = @prompt.mask("Please try to insert your password again:".red)
-        if @user.authenticate(password)
-        else
-          sleep(1)
-          puts "The password you entered is incorrect.".red
-          puts "Please return later.".red
-          system exit
-        end
-      end
+  def continue_play?
+    if @prompt.yes?("Would you like to continue playing?".blue)
+      formulate_question
     else
-      sleep(1)
       puts ""
-      puts "We're sorry but we couldn't retrieve your account.".red
+      puts "Thanks for playing!".yellow
       puts ""
-      if @prompt.yes?("Would you like to create a new account?".green)
-        create_account
-      else
       start_menu
     end
-    end
+  end
 
-    choice = @prompt.select("Please choose from the following options:") do |menu|
-      menu.choice 'Change my name'
-      menu.choice 'Change my password'
-      menu.choice 'Delete my account'
-      menu.choice "Back to Main Menu"
+# Provides roadmap for account management
+  def manage_account
+    if @user
+      manage_account_menu
+    else
+      login
+      manage_account_menu
     end
+  end
+
+# Gives options to manage account
+def manage_account_menu
+  choice = @prompt.select("Please choose from the following options:") do |menu|
+    menu.choice 'Change my name'
+    menu.choice 'Change my password'
+    menu.choice 'Delete my account'
+    menu.choice "Back to Main Menu"
+  end
 
   if choice == "Change my name"
-    new_name = @prompt.ask("What's your name?")
-    @user.name = new_name
-    @user.save
-    puts "Welcome, #{new_name}"
-    start_menu
+    change_name
   elsif choice == "Change my password"
-    new_password = @prompt.mask("Please set a new password:".green)
-    new_password_reenter = @prompt.mask("Please reenter your new password:".green)
-    @user.password = new_password
-    @user.save
-    puts ""
-    puts "Your password has been successfully changed.".green
-    puts ""
-    start_menu
+    change_password
   elsif choice == "Delete my account".blue
-    if @prompt.yes?("Are you sure you want to delete your account. ALL YOUR POINTS AND PROGRESS WILL BE LOST!".red)
-      @user.destroy
-    else
-      manage_account
-    end
+    delete_account
   elsif choice == "Back to Main Menu"
     start_menu
   end
+end
 
+def change_name
+  new_name = @prompt.ask("What's your name?")
+  @user.name = new_name
+  @user.save
+  puts ""
+  puts "Your name was changed to #{new_name}."
+  puts ""
+  manage_account_menu
+end
+
+def change_password
+  new_password = @prompt.mask("Please set a new password:".green)
+  new_password_reenter = @prompt.mask("Please reenter your new password:".green)
+  if new_password == new_password_reenter
+    @user.password = new_password
+    @user.save
+    puts ""
+    puts "Your password has been successfully changed.".blue
+    puts ""
+    manage_account_menu
+  else
+    puts "The pass do not match."
+    new_password_reenter = @prompt.mask("Please reenter your new password:".green)
+    if new_password == new_password_reenter
+      @user.password = new_password
+      @user.save
+      puts ""
+      puts "Your password has been successfully changed.".blue
+      puts ""
+      manage_account_menu
+    else
+      puts ""
+      puts "Your passwords do not match."
+      puts "Please try again later."
+      manage_account_menu
+    end
+  end
+end
+
+def delete_account
+  if @prompt.yes?("Are you sure you want to delete your account. ALL YOUR POINTS AND PROGRESS WILL BE LOST!".red)
+    @user.destroy
+  else
+    manage_account
+  end
 end
 
 # Creates a new user account.
 
 def create_account
-  "
-  Let's create your account!
-  "
+  puts ""
+  puts "Let's create your account!"
+  puts ""
   name = @prompt.ask("What's your name?")
   email = @prompt.ask("What's your email?")
   password = @prompt.mask("Please create a password:".green)
@@ -341,4 +321,5 @@ end
     display_splash_text
     start_menu
   end
+
 end
